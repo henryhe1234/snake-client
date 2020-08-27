dd963eedb230d2172ec2622cfe4462c91936ed0c
henryhe1234 <henryhe1123@gmail.com>
2020-08-27 01:21:57 +0000

    create stdin for user input

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ const connect = function() {
   conn.on('connect',()=>{
     console.log(`Successfully connected to game server`);
     conn.write("Name: ZXH");
+   
   });
   conn.on('data',(data)=>{
     console.log(`->${data}`);
